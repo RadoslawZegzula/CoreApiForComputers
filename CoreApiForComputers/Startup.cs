5912d41c89f08c50e1df64285182fb2984a46439
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-05-25 12:18:04 +0200

    Add XML

--- CoreApiForComputers/Startup.cs
+++ CoreApiForComputers/Startup.cs
@@ -16,7 +16,7 @@ namespace CoreApiForComputers
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
