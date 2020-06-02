26e9f019d6a615763392d96f8f4f0a89935ca979
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-06-02 22:23:17 +0200

    Add Entity Core Repository

---
+++ CoreApiForComputers/DataBase/EntityContext.cs
@@ -0,0 +1,35 @@
+﻿using CoreApiForComputers.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection; //<-- part of the new logging implementation
+using Microsoft.Extensions.Logging;
+
+namespace CoreApiForComputers.DataBase
+{
+    public class EntityContext : DbContext
+    {
+        private readonly ILoggerFactory MyConsoleLoggerFactory;
+        public DbSet<CpuEntity> CpuEntityContext { get; set; }
+
+        public EntityContext()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder => builder
+                        .AddConsole()
+                        .AddFilter
+                        (DbLoggerCategory.Database.Command.Name, LogLevel.Information));
+
+            MyConsoleLoggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(MyConsoleLoggerFactory).EnableSensitiveDataLogging(true).UseSqlServer("Connection string");
+
+        }
+
+    }
+}
---
+++ CoreApiForComputers/DataBase/EntityRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiForComputers.DataBase
+{
+    public class EntityRepository : IRepository
+    {
+        private static readonly EntityContext _context = new EntityContext();
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Read<T>()
+        {
+            return (IEnumerable<T>) _context.CpuEntityContext;
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
