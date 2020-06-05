724086945a2aaafe2a8e516c2037ec8cfe96ca94
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-06-05 20:17:14 +0200

    Document CpuEntity class

--- CoreApiForComputers/Models/Entities/CpuEntity.cs
+++ CoreApiForComputers/Models/Entities/CpuEntity.cs
@@ -5,13 +5,30 @@ using System.Threading.Tasks;
 
 namespace CoreApiForComputers.Models.Entities
 {
+    /// <summary>
+    /// The model of central_processing_unit which purpose is to be stored and retrieved from database
+    /// </summary>
     public class CpuEntity
     {
+        /// <summary>
+        /// The Id of central processing unit
+        /// </summary>
         public int Id { get; set; }
-
+        /// <summary>
+        /// The frequency of central_processing_unit
+        /// </summary>
         public int Frequency { get; set; }
+        /// <summary>
+        /// The number of cores in the central_processing_unit
+        /// </summary>
         public int Cores { get; set; }
+        /// <summary>
+        /// The required motherboard socket for central_processing_unit
+        /// </summary>
         public string Socket { get; set; }
+        /// <summary>
+        /// The thermal_design_power of central_processing_unit
+        /// </summary>
         public float Tdp { get; set; }
     }
 }
