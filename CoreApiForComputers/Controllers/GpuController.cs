349ef4631063a56fdd3daf2e61052c1fb531b891
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-06-17 21:04:41 +0200

    Add GpuController class

---
+++ CoreApiForComputers/Controllers/GpuController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiForComputers.Controllers
+{
+    /// <summary>
+    /// Controller for graphics processing unit
+    /// </summary>
+    [ApiController]
+    [Route("api/v{version:apiVersion}/cpus")]
+    [Produces("application/xml")]
+    public class GpuController : ControllerBase
+    {
+
+    }
+}
