d4776c577c4dc439213acb4657b2c393278c9d6e
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-06-17 22:31:38 +0200

    Add Gpu Entity

---
+++ CoreApiForComputers/Models/Entities/GpuEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiForComputers.Models.Entities
+{
+    /// <summary>
+    /// The model of graphics_processing_unit which purpose is to be stored and retrieved from database
+    /// </summary>
+    public class GpuEntity : BaseEntityOfParts
+    {
+
+    }
+}
