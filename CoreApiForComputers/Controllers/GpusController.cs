33211a8ab4adb7a6d66325c1bba94317915300d6
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-07-03 15:40:17 +0200

    Add exceptions

--- CoreApiForComputers/Controllers/GpusController.cs
+++ CoreApiForComputers/Controllers/GpusController.cs
@@ -3,6 +3,7 @@ using CoreApiForComputers.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace CoreApiForComputers.Controllers
@@ -25,7 +26,7 @@ namespace CoreApiForComputers.Controllers
         /// <param name="data"></param>
         public GpusController(IGenericRepository<GpuEntity> data)
         {
-            database = data;
+            database = data ?? throw new ArgumentNullException(nameof(data));
         }
 
         /// <summary>
